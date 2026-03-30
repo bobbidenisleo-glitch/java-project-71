c8059cf1bd0a616cd15715a6c2d6430d8de59353
bobbidenisleo-glitch <bobbidenisleo@gmail.com>
2026-03-30 16:13:30 +0300

    Restore build.gradle.kts with correct checkstyle path

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,41 +1,30 @@
 plugins {
-    application
-    checkstyle
-    jacoco
+    id("application")
+    id("checkstyle")
+    id("jacoco")
+    id("com.github.ben-manes.versions") version "0.51.0"
     id("org.sonarqube") version "4.4.1.3373"
-}
-
-application {
-    mainClass.set("hexlet.code.App")
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "hexlet.code.App"
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.16.1")
-    implementation("info.picocli:picocli:4.7.5")
-    annotationProcessor("info.picocli:picocli-codegen:4.7.5")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.2")
+    implementation("info.picocli:picocli:4.7.6")
     
-    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
-}
-
-tasks.compileJava {
-    options.release = 17
-    options.encoding = "UTF-8"
-    options.compilerArgs.add("-Aproject=${project.group}/${project.name}")
+    testImplementation("org.assertj:assertj-core:3.25.3")
 }
 
 tasks.test {
@@ -43,40 +32,24 @@ tasks.test {
     finalizedBy(tasks.jacocoTestReport)
 }
 
-checkstyle {
-    toolVersion = "10.12.1"
-    configFile = file("${project.rootDir}/config/checkstyle/checkstyle.xml")
-}
-
-jacoco {
-    toolVersion = "0.8.11"
-}
-
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
     reports {
-        xml.required = true
-        html.required = true
+        xml.required.set(true)
+        csv.required.set(false)
+        html.required.set(true)
     }
 }
 
-sonarqube {
-    properties {
-        property("sonar.projectKey", "java-project-71")
-        property("sonar.projectName", "java-project-71")
-        property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.organization", "YOUR_ORG_NAME")
-        property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
-        property("sonar.sourceEncoding", "UTF-8")
-    }
+checkstyle {
+    toolVersion = "10.12.0"
+    configFile = file("config/checkstyle/checkstyle.xml")
+    configProperties["checkstyle.cache.file"] = file("build/checkstyle/cache")
 }
 
-sonarqube {
-    properties {
-        property("sonar.projectKey", "java-project-71")
-        property("sonar.projectName", "java-project-71")
-        property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.organization", "YOUR_ORG_NAME")
-        property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
+tasks.withType<Checkstyle> {
+    reports {
+        xml.required.set(false)
+        html.required.set(true)
     }
 }
