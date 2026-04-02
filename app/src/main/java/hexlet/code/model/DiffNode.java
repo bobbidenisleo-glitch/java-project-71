1ffce912e5ef1188863d966751fcc4c3958c4100
bobbidenisleo-glitch <bobbidenisleo@gmail.com>
2026-04-02 13:11:17 +0300

    Update checkstyle config and fix formatting

--- app/src/main/java/hexlet/code/model/DiffNode.java
+++ app/src/main/java/hexlet/code/model/DiffNode.java
@@ -1,21 +1,38 @@
 package hexlet.code.model;
 
-@SuppressWarnings("checkstyle:LeftCurly")
-public final class DiffNode {
+public final class DiffNode 
+{
     private String key;
     private DiffType type;
     private Object oldValue;
     private Object newValue;
 
-    public DiffNode(String key, DiffType type, Object oldValue, Object newValue) {
+    public DiffNode(String key, DiffType type, Object oldValue, Object newValue) 
+    {
         this.key = key;
         this.type = type;
         this.oldValue = oldValue;
         this.newValue = newValue;
     }
 
-    public String getKey() { return key; }
-    public DiffType getType() { return type; }
-    public Object getOldValue() { return oldValue; }
-    public Object getNewValue() { return newValue; }
+    public String getKey() 
+    {
+        return key;
+    }
+
+    public DiffType getType() 
+    {
+        return type;
+    }
+
+    public Object getOldValue() 
+    {
+        return oldValue;
+    }
+
+    public Object getNewValue() 
+    {
+        return newValue;
+    }
 }
+
